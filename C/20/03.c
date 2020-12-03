c75a9d0b59a43b37047cdd026320ea7fc099b79e
Joseph Novakovich <josephnovakovich@gmail.com>
2020-12-03 18:07:50 -0500

    day 3 in C lol

---
+++ C/20/03.c
@@ -0,0 +1,13 @@
+#include "aoc.h"
+#define H 323
+#define W 31
+C T[H][W];
+
+I ski (I dx, I dy) { I t=0;DO(H/dy,t+='#'==T[i*dy][i*dx%W]);R t; }
+
+int main ()
+{ C*p=(C*)T;
+  while (EOF!=scanf("%s\n",p)) p+=W;
+  printf("part A: %lld\n",ski(3,1));
+  printf("part B: %lld\n",ski(1,1)*ski(3,1)*ski(5,1)*ski(7,1)*ski(1,2));
+  R 0; }
