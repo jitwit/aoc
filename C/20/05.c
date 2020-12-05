73cd6a1bf05ff6cb0c1abcd90a305a1091e2f6d6
Joseph Novakovich <josephnovakovich@gmail.com>
2020-12-05 02:35:06 -0500

    only need hi

--- C/20/05.c
+++ C/20/05.c
@@ -3,11 +3,11 @@
 C B[10];I S[1024];
 
 int main ()
-{ I lo=1024,hi=0,x;
+{ I hi=-1,x;
   while (EOF!=scanf("%s",B))
-    { x=0; DO(10,x*=2;x+=(B[i]=='B')+(B[i]=='R'));
-      lo=MIN(lo,x);hi=MAX(hi,x);S[x]=1; }
+    { x=0;DO(10,x*=2;x+=(B[i]=='B')+(B[i]=='R'));
+      hi=MAX(hi,x);S[x]=1; }
   printf("Part A: %lld\n",hi);
-  x=lo; while (S[x]) x++;
+  x=hi; while (S[x]) x--;
   printf("Part B: %lld\n",x);
   R 0; }
