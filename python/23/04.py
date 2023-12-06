7d5ce464dd84f0bc84182d6b2d486da31c453bdd
jitwit <josephnovakovich@gmail.com>
2023-12-06 12:46:37 -0500

    minorly

--- python/23/04.py
+++ python/23/04.py
@@ -1,16 +1,14 @@
 import numpy as np
 
-X = []  # counts for each hand
+X = []
 for l in open('../../input/2023/4.in','r').readlines():
-    tmp = " ".join(l.split()[2:]).split('|')
-    [a,b] = [ [ int(y) for y in x.split() ] for x in tmp ]
+    t = " ".join(l.split()[2:]).split('|')
+    [a,b] = [ [ int(y) for y in x.split() ] for x in t ]
     X.append(len(set(a) & set(b)))
-
-print(sum((2**(np.array(X))) >> 1))
+print(sum(2**(np.array(X)) >> 1))
 
 Y = [1]*len(X)
 for i,x in enumerate(X):
     for j in range(i,x+i):
         Y[j+1] += Y[i]
-
 print(sum(Y))
