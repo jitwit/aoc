f1c5b71fd98bbb813597bb25deb8d914b46907cb
jitwit <josephnovakovich@gmail.com>
2023-12-06 12:08:34 -0500

    day 4

---
+++ python/23/04.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+X = []  # counts for each hand
+for l in open('../../input/2023/4.in','r').readlines():
+    tmp = " ".join(l.split()[2:]).split('|')
+    [a,b] = [ [ int(y) for y in x.split() ] for x in tmp ]
+    X.append(len([ x for x in b if x in a ]))
+
+print(sum((2**(np.array(X))) >> 1))
+
+Y = [1]*len(X)
+for i,x in enumerate(X):
+    for j in range(i,x+i):
+        Y[j+1] += Y[i]
+
+print(sum(Y))
