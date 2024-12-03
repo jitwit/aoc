a037bfe423c832307202643cd059b6dc7cb42457
jitwit <josephnovakovich@gmail.com>
2024-12-03 10:52:59 -0500

    part b!

--- elisp/24/03.el
+++ elisp/24/03.el
@@ -1,17 +1,29 @@
 (with-temp-buffer
   (insert-file-contents "~/code/aoc/input/2024/3.in")
-  (let ((mul (rx (seq "mul("
-		      (group (+ digit))
-		      ","
-		      (group (+ digit))
-		      ")")))
-	(a 0))
+  (let ((mul (rx (or (seq "mul("
+			  (group (+ digit))
+			  ","
+			  (group (+ digit))
+			  ")")
+		     "do()"
+		     "don't()")))
+	(a 0)
+	(b 0)
+	(add-to-b? t))
     (while (re-search-forward mul nil t)
-      (setq a
-	    (+ a
-	       (* (string-to-number
-		   (buffer-substring (match-beginning 1) (match-end 1)))
-		  (string-to-number
-		   (buffer-substring (match-beginning 2) (match-end 2)))))))
-    (gui-select-text (number-to-string a)) ;; woah
-    a))
+      (let ((match (match-string 0)))
+	(cond ((string-equal match "do()")
+	       (setq add-to-b? t))
+	      ((string-equal match "don't()")
+	       (setq add-to-b? nil))
+	      (t
+	       (let ((x (string-to-number
+			 (buffer-substring (match-beginning 1) (match-end 1))))
+		     (y (string-to-number
+			 (buffer-substring (match-beginning 2) (match-end 2)))))
+		 (setq a (+ a (* x y)))
+		 (when add-to-b?
+		   (setq b (+ b (* x y)))))))))
+    (gui-select-text (number-to-string b)) ;; woah
+    (list a b)))
+
