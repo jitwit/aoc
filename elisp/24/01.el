00c1df5708f4fc9e18d10335780a1f4d10fe26a8
jitwit <josephnovakovich@gmail.com>
2024-12-05 19:52:46 -0500

    minorly

--- elisp/24/01.el
+++ elisp/24/01.el
@@ -3,7 +3,7 @@
 (with-aoc-input
   (let ((input (mapcar #'string-to-number (split-string (buffer-string)))))
     (emacs->J WWJ "in" input)
-    (let ((a (J-set&get "+/ | -/ /:~ \"1 in =: |: _2 ]\\ in"))
+    (let ((a (J-set&get "+/|-//:~\"1 in=:|:_2]\\ in"))
 	  (b (J-set&get "([:+/[*[:+/=/~)/ in")))
       (gui-select-text (number-to-string b))
       (list a b))))
