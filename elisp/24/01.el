5bad65840850a6a1c54982eb2cb576ca4c615e0f
jitwit <josephnovakovich@gmail.com>
2024-12-08 14:49:40 -0500

    some comments in cursed emacs solution lol

--- elisp/24/01.el
+++ elisp/24/01.el
@@ -2,8 +2,8 @@
 ;; to do the computations.
 (with-aoc-input
   (let ((input (mapcar #'string-to-number (split-string (buffer-string)))))
-    (emacs->J WWJ "in" input)
-    (let ((a (J-run "+/|-//:~\"1 in=:|:_2]\\ in"))
-	  (b (J-run "([:+/[*[:+/=/~)/ in")))
-      (gui-select-text (number-to-string b))
+    (emacs->J WWJ "in" input) ; send input to J
+    (let ((a (J-run "+/|-//:~\"1 in=:|:_2]\\ in")) ; calculate part a with J
+	  (b (J-run "([:+/[*[:+/=/~)/ in")))       ; calculate part b with J
+      (gui-select-text (number-to-string b))       ; grab to system clipboard
       (list a b))))
