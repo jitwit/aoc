b65527185971b21b64781954a31e43eff82d1a79
jitwit <josephnovakovich@gmail.com>
2024-12-23 23:49:36 -0500

    some haskell

---
+++ haskell/24/23.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Algebra.Graph.AdjacencyMap
+import qualified Data.Set as S
+import Data.Tuple
+import Data.List
+import Advent
+
+adj g = S.toList . postSet g
+
+part'a g = length $ nub triangles where
+  triangles = nub [ sort [a,b,c]
+                  | a@('t':_) <- vertexList g
+                  , b <- adj a g
+                  , c <- adj b g
+                  , hasEdge c a g ]
+
+part'b g = concat $ intersperse "," $ sort max'clique where
+  max'clique = maximumBy (\a b -> compare (length a) (length b)) cliques
+  cliques = [ expand'clique g v | v <- vertexList g ]
+  
+expand'clique g x = foldr expand [x] $ adj x g where
+  expand v c | all id [ hasEdge v x g | x <- c ] = v:c
+             | otherwise = c
+
+main =
+  do ls <- map (\l -> (take 2 l,drop 3 l)) . lines <$> input'string 24 23
+     let g = edges $ ls ++ map swap ls
+     output $ AB (part'a g) (part'b g)
