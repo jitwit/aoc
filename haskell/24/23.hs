b69a4bc69ada48130c57af6b783e10a126a0eb76
jitwit <josephnovakovich@gmail.com>
2024-12-24 18:40:29 -0500

    minorly

--- haskell/24/23.hs
+++ haskell/24/23.hs
@@ -2,17 +2,15 @@ module Main where
 
 import Algebra.Graph.AdjacencyMap
 import qualified Data.Set as S
-import Data.Tuple
 import Data.List
 import Data.Ord
 import Advent
 
-part'a g = length $ nub triangles where
-  triangles = [ sort [a,b,c]
-              | a@('t':_) <- vertexList g
-              , b <- S.elems $ postSet a g
-              , c <- S.elems $ postSet b g
-              , hasEdge c a g ]
+part'a g = length $ nub [ sort [a,b,c]
+                        | a@('t':_) <- vertexList g
+                        , b <- S.elems $ postSet a g
+                        , c <- S.elems $ postSet b g
+                        , hasEdge c a g ]
 
 part'b g = intercalate "," $ S.elems max'clique where
   max'clique = maximumBy (comparing length) cliques
@@ -22,6 +20,6 @@ part'b g = intercalate "," $ S.elems max'clique where
   cliques = [ expand'clique g v | v <- vertexList g ]
 
 main =
-  do ls <- map (\l -> (take 2 l, drop 3 l)) . lines <$> input'string 24 23
-     let g = edges $ ls ++ map swap ls
+  do ls <- map (fmap tail.splitAt 2) . lines <$> input'string 24 23
+     let g = edges $ ls ++ map (\(x,y) -> (y,x)) ls
      output $ AB (part'a g) (part'b g)
