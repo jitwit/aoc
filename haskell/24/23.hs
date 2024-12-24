667261f18076e229f9bc7ad7b9ae90903b561142
jitwit <josephnovakovich@gmail.com>
2024-12-24 03:52:27 -0500

    minorly

--- haskell/24/23.hs
+++ haskell/24/23.hs
@@ -7,22 +7,19 @@ import Data.List
 import Data.Ord
 import Advent
 
-adj g = S.elems . postSet g
-
 part'a g = length $ nub triangles where
   triangles = nub [ sort [a,b,c]
                   | a@('t':_) <- vertexList g
-                  , b <- adj a g
-                  , c <- adj b g
+                  , b <- S.elems $ postSet a g
+                  , c <- S.elems $ postSet b g
                   , hasEdge c a g ]
 
 part'b g = intercalate "," $ max'clique where
   max'clique = maximumBy (comparing length) cliques
-  cliques = [ expand'clique g v | v <- vertexList g ]
-
-expand'clique g x = foldr expand [x] $ adj x g where
   expand v c | all id [ hasEdge v x g | x <- c ] = v:c
              | otherwise = c
+  expand'clique g x = foldr expand [x] $ S.elems $ postSet x g
+  cliques = [ expand'clique g v | v <- vertexList g ]
 
 main =
   do ls <- map (\l -> (take 2 l, drop 3 l)) . lines <$> input'string 24 23
