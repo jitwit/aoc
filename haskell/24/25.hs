545e90c8eea19b097e23b22fa6ab43b7a939fe24
jitwit <josephnovakovich@gmail.com>
2024-12-25 04:03:30 -0500

    minorly

--- haskell/24/25.hs
+++ haskell/24/25.hs
@@ -8,9 +8,9 @@ import Advent
 main =
   do input <- map (fromEnum.(=='#')) . filter (/='\n') <$> input'string 24 25
      let (keys,locks) = partition ((==1).head.concat) $ chunks 7 $ chunks 5 input
-         heights = map (map (pred.sum) . transpose)
+         height = map (pred.sum) . transpose
          chunks n = unfoldr $ \case [] -> Nothing
                                     xs -> Just $ splitAt n xs
-     print $ sum [ 1 | k <- heights keys
-                     , l <- heights locks
-                     , all (<=5) $ zipWith (+) k l ]
+     print $ sum [ 1 | h'k <- height <$> keys
+                     , h'l <- height <$> locks
+                     , all (<=5) $ zipWith (+) h'k h'l ]
